cd9564be7c360af084cb6b47dfdad16fc4fd02d3
Parth Sane <laerdevstudios@gmail.com>
2016-03-09 18:04:51 +0530

    Multiple Tags Read Easily!

--- app/src/main/java/in/laer/moodle/MainActivity.java
+++ app/src/main/java/in/laer/moodle/MainActivity.java
@@ -1,13 +1,23 @@
 package in.laer.moodle;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                new DownloadWebPageTask().execute();
+                Snackbar.make(view, "Fetching Notices", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -47,5 +58,63 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+
+        // ProgressDialog dialog = new ProgressDialog(getParent());
+        // TextView txtView;
+        // The variable is moved here, we only need it here while displaying the
+        // progress dialog.
+
+
+        @Override
+        protected void onPreExecute() {
+            //   dialog.show();
+            //   dialog.setContentView(R.layout.activity_main);
+            // Set the variable txtView here, after setContentView on the dialog
+            // has been called! use dialog.findViewById().
+            //   txtView = (TextView)dialog.findViewById(R.id.txtView);
+        }
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+            StringBuffer sb = new StringBuffer();
+            try {
+                List<Element> notices = new ArrayList<>();
+// Get The Site and Parse it
+                Document document = Jsoup.connect("http://moodle.siesgst.ac.in").get();
+                notices.addAll(document.select(".forumpost").addClass("posting"));
+                // Select Table
+
+                for (Element notice : notices) {
+                    Iterator<Element> lines = notice.select("td").addClass("posting").iterator();
+                    while (lines.hasNext()) {
+                        Element line = lines.next();
+
+                        sb.append(line.text());
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return sb.toString();
+        }
+
+        @Override
+        protected void onPostExecute(final String result) {
+
+            // TODO:set textview
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //      txtView.setText(result);
+                }
+            });
+        }
     }
 }
